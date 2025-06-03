ce469b25e6c80a0a64e1b39c7ec2dbd827042862
tamir.rustamov <timorust1@gmail.com>
2025-06-03 10:02:18 +0300

    corr key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+set -e
 
-set -e 
+if [ -z "$SECRET_KEY" ]; then
+  echo "⚠️ SECRET_KEY not found. Generating temporary key..."
+  export SECRET_KEY=$(python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())")
+fi
 
-echo "📦 Running migrations..."
+echo "📦 Running database migrations..."
 python manage.py migrate
 
-echo "📚 Updating catalog (may take a few minutes)..."
+echo "📚 Downloading or updating catalog..."
 python manage.py updatecatalog
 
 echo "🎨 Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "🚀 Starting server..."
-exec "$@"
+echo "🚀 Starting Django development server..."
+exec python manage.py runserver 0.0.0.0:8000
